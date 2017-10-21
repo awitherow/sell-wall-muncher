9111495c2f07caa57ded31d4bb9bb3b5668dbab2
awitherow <au.witherow@gmail.com>
2017-10-21 12:02:55 +0200

    get lowest sell and get randomized buy amount

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ var client = new Yobit({
   key: process.env.KEY,
   secret: process.env.SECRET
 });
+
+client.getOrderBook(function(error, data) {
+  if (error) {
+    console.log("[ERR]: ", error);
+    return;
+  }
+
+  console.log("[INFO] lowest sell order ->", data.buzz_btc.asks[0][0]);
+  console.log("[INFO] purchasing ", getRandomInt(500, 2500), " BUZZ");
+}, "buzz_btc");
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
