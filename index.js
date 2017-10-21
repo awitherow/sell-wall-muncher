75d5dbc61b062c58822496bf7146be7a0a230e4f
awitherow <au.witherow@gmail.com>
2017-10-21 14:08:09 +0200

    add buy order

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ if (process.env.ENV === "dev") {
   require("dotenv").config();
 }
 
-var client = new Yobit({
-  key: process.env.KEY,
-  secret: process.env.SECRET
-});
+var client = new Yobit(process.env.KEY, process.env.SECRET);
 
-client.getOrderBook(function(error, data) {
+client.getOrderBook(function(error, response) {
   if (error) {
     console.log("[ERR]: ", error);
     return;
   }
 
-  var lowestSellOrder = data.buzz_btc.asks[0][0];
-  var sellWallInCoin = data.buzz_btc.asks[0][1];
+  var lowestSellOrder = Number(response.buzz_btc.asks[0][0]);
+  var sellWallInCoin = response.buzz_btc.asks[0][1];
   var percentPurchaseMinimum = 0.0001;
   var percentPurchaseMaximum = 0.00025;
   var randomPurchasePrice = getRandomInt(
@@ -24,9 +21,21 @@ client.getOrderBook(function(error, data) {
     sellWallInCoin * percentPurchaseMaximum
   );
 
-  console.log("[INFO] lowest sell order ->");
+  console.log("[INFO] lowest sell order ->", lowestSellOrder);
   console.log("[INFO] purchasing ", randomPurchasePrice, " BUZZ");
-  // place buy order
+
+  client.addTrade(
+    function(success, response) {
+      if (!success) {
+        console.log("[ERR]: ", response);
+        return;
+      }
+    },
+    "buzz_btc",
+    "buy",
+    lowestSellOrder,
+    randomPurchasePrice
+  );
 }, "buzz_btc");
 
 function getRandomInt(min, max) {
