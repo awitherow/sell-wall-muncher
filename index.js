46b5b97c2537849992f5b8244629652d1f24c7ca
awitherow <au.witherow@gmail.com>
2017-10-21 12:06:41 +0200

    variablize it

--- index.js
+++ index.js
@@ -15,10 +15,20 @@ client.getOrderBook(function(error, data) {
     return;
   }
 
-  console.log("[INFO] lowest sell order ->", data.buzz_btc.asks[0][0]);
-  console.log("[INFO] purchasing ", getRandomInt(500, 2500), " BUZZ");
+  var lowestSellOrder = data.buzz_btc.asks[0][0];
+  var sellWallInCoin = data.buzz_btc.asks[0][1];
+  var percentPurchaseMinimum = 0.0001;
+  var percentPurchaseMaximum = 0.00025;
+  var randomPurchasePrice = getRandomInt(
+    sellWallInCoin * percentPurchaseMinimum,
+    sellWallInCoin * percentPurchaseMaximum
+  );
+
+  console.log("[INFO] lowest sell order ->");
+  console.log("[INFO] purchasing ", randomPurchasePrice, " BUZZ");
+  // place buy order
 }, "buzz_btc");
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return parseInt(Math.floor(Math.random() * (max - min + 1)) + min);
 }
